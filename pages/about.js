084ef807f5468f72e072cdef487e3bb18bc74128
sangam3079 <sangam3079@gmail.com>
2021-05-19 11:51:30 +0545

    first commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+      <>
+        <Head>
+          <title>next js | about</title>
+          <meta name='keywords' content='next js' />
+        </Head>
+        <div>
+          <h1>About</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        </div>
+      </>
+    );
+  }
+   
+  export default About;
